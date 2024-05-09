884983a871c1458c5c28b7da641769ba2bc7d00b
Avishka <avishkak29@gmail.com>
2024-05-09 15:53:05 +0530

    Updated

--- chat-app/backend/index.js
+++ chat-app/backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'chat-app-w3villa.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
