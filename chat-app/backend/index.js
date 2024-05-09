ebe981c87e4d35aa8d44379c3ed7d4760e9f3d08
Avishka <90856769+Aviii9@users.noreply.github.com>
2024-05-09 18:32:01 +0530

    Update index.js

--- chat-app/backend/index.js
+++ chat-app/backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://classy-klepon-41c7f7.netlify.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
